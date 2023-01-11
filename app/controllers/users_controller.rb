eb15c04eed84b1a48787a455cb650ae8718ab799
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-12 01:49:43 +0300

    configured show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
             render json: { errors:  user.errors.full_message}
     end
 
+    #show route 
+    def show
+        current_user = @current_user
+        render json: current_user, status: :ok
+    end
+
+
     private 
     def user_params
         params.permit(:username,:password, :email, :password_confirmation)
