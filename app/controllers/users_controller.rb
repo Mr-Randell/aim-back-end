aa6a079e55c75a7851be4b2080149ea63f08bc3b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-12 01:44:06 +0300

    Authenticationg user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    #create
+    def create
+        user = User.create(user_params)
+        #finding a user in the database using the username
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else 
+            render json: { errors:  user.errors.full_message}
+    end
+
+    private 
+    def user_params
+        params.permit(:username,:password, :email, :password_confirmation)
+    end
+
+
 end
