111cfe6662d88c99820a68cc6fdfb6d4c7d25ca8
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-12 01:27:23 +0300

    Code refactored

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,5 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :status, :urgency, :user_id, :asset_id,  :quantity
+  attributes :id, :name, :category, :status, :urgency, :user_id, :asset_id,:quantity
   belongs_to :user 
   belongs_to :asset 
 end
