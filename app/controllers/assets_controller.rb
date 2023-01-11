ca20be0332c67c420d256c81347b107a50c07e4b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-12 02:51:37 +0300

    Configured our create method

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,2 +1,8 @@
 class AssetsController < ApplicationController
+    
+    def create
+        new_asset = Asset.create!(asset_params)
+        render json: new_asset, status: :created
+    end
+
 end
