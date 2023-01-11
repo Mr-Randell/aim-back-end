4d9c570654bfe3def51e0049c3d83b27076b07af
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-12 02:56:01 +0300

    Defining our Show method

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,8 +1,20 @@
 class AssetsController < ApplicationController
-    
+
     def create
         new_asset = Asset.create!(asset_params)
         render json: new_asset, status: :created
     end
+
+    def show
+        asset = find_asset(params[:id])
+        render json: asset, include: [:user]
+    end
+
+
+    private
+    def asset_params
+        params.permit(:name, :quantity)
+    end
+
 
 end
