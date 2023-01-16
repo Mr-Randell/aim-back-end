7872abb5b73d3cddec5a1396b23b68a5eda28ff9
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-16 07:49:53 +0300

    Create Employees migration refactored

--- db/migrate/20230116044630_create_employees.rb
+++ db/migrate/20230116044630_create_employees.rb
@@ -3,9 +3,9 @@ class CreateEmployees < ActiveRecord::Migration[7.0]
     create_table :employees do |t|
       t.integer :employee_id
       t.string :employee_name
-      t.date :date_of_birth
-      t.date :joining_date
-      t.date :leaving_date
+      t.string :date_of_birth
+      t.string :joining_date
+      t.string :leaving_data
       t.string :password_digest
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_223044) do
-  create_table "assets", force: :cascade do |t|
-    t.string "user_name"
-    t.integer "user_id"
-    t.string "description"
-    t.boolean "status"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "price"
-    t.string "released_year"
-    t.integer "employee_id"
-    t.integer "category_id"
-  end
-
-  create_table "requests", force: :cascade do |t|
-    t.string "user_name"
-    t.string "category"
-    t.boolean "status"
-    t.string "urgency"
-    t.integer "user_id"
-    t.integer "asset_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "email"
-    t.string "role"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_223556) do
 end
