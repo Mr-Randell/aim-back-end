77de1beb8ae4307185d60fe12511f3a38a8f6728
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-18 21:55:11 +0300

    Added CompanyName, PhoneNumber,CompanySize and Industry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_134459) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_185126) do
   create_table "assets", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -57,6 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_134459) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "companyname"
+    t.integer "phonenumber"
+    t.integer "companysize"
+    t.string "industry"
   end
 
 end
