a26e1e07f659e29e48e2527cc99e4d4ebe249da1
Patrobas Bwire <patrobasbwire@gmail.com>
2023-01-19 09:59:52 +0300

    validation changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     # belongs_to :assets, optional: true
      has_many :assets
 
-    # validates :category, presence: true,
+    validates :category, presence: true,
 end
